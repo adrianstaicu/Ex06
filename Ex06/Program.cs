dc2d7f54fa7fff4438dc0a6292f01c4efe3f70ff
Staicu Adrian <adrianstacu888@gmail.com>
2020-02-03 23:26:14 +0200

    Add project files.

---
+++ Ex06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 9;
+            int c = 5;
+
+            if (a < b && a < c) {
+                Console.WriteLine(" a este cel mai mic");
+                    }
+            else if (b < a && b < c) {
+                Console.WriteLine(" b este cel mai mic");
+                    }
+            else
+            {
+                Console.WriteLine(" c este cel mai mic");
+            }
+        }
+    }
+}
